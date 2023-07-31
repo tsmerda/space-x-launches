70330defcaaefc0bd7c1ea58aecbd595f7fd4fbb
Tomáš Šmerda <tsmerda2@gmail.com>
2023-07-31 17:46:40 +0200

    feat: app structure and set up

---
+++ SpaceXLaunches/Views/ListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ListViewController.swift
+//  SpaceXLaunches
+//
+//  Created by Tomáš Šmerda on 31.07.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+    
+}
+
+private extension ListViewController {
+    
+    func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.title = "Launches"
+        self.view.backgroundColor = .white
+    }
+    
+}
